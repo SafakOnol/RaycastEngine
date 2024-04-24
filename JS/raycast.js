43e9a7ee195fb872e50e0b9a729d3bb3c5070e4e
SafakOnol <onolsafak@outlook.com>
2024-04-23 21:37:58 -0400

    add DDA-Algorithm to find Vertical and Horizontal Intersections

--- JS/raycast.js
+++ JS/raycast.js
@@ -11,6 +11,12 @@ const WINDOW_HEIGHT = MAP_NUM_ROWS * TILE_SIZE;
 
 const PLAYER_DIRECTION_LENGTH = 30;
 
+const FOV = 60 * (Math.PI / 180);
+
+const WALL_STRIP_WIDTH = 30; // Wall thickness in px
+const NUM_RAYS = WINDOW_WIDTH / WALL_STRIP_WIDTH;
+
+
 // ----- PROJECT CLASSES ----- // 
 class MyMap
 {
@@ -75,7 +81,7 @@ class Player
         this.walkDirection = 0;  // -1 if back, +1 if front
         this.rotationAngle = Math.PI / 2;
         this.moveSpeed = 2.0;
-        this.rotationSpeed = 10 * (Math.PI / 180); // 2 degrees converted to Radians
+        this.rotationSpeed = 4 * (Math.PI / 180); // 2 degrees converted to Radians
     }
 
     update()
@@ -106,22 +112,88 @@ class Player
         fill("red");
         circle(this.x, this.y, this.radius);
         // Player Direction Indicator
-        stroke("red");
-        line
-        (
-            this.x,
-            this.y,
-            this.x + Math.cos(this.rotationAngle) * PLAYER_DIRECTION_LENGTH,
-            this.y + Math.sin(this.rotationAngle) * PLAYER_DIRECTION_LENGTH
-        );
+        // stroke("red");
+        // line
+        // (
+        //     this.x,
+        //     this.y,
+        //     this.x + Math.cos(this.rotationAngle) * PLAYER_DIRECTION_LENGTH,
+        //     this.y + Math.sin(this.rotationAngle) * PLAYER_DIRECTION_LENGTH
+        // );
     }
 }
 
+class Ray
+{
+    constructor(rayAngle)
+    {
+        this.rayAngle = normalizeAngle(rayAngle);
+        this.wallHitX = 0;
+        this.wallHitY = 0;
+        this.distance = 0;
+
+        this.isRayFacingDown    = this.rayAngle > 0 && this.rayAngle < Math.PI;
+        this.isRayFacingUp      = !this.isRayFacingDown;
+        this.isRayFacingRight   = this.rayAngle < 0.5 * Math.PI || this.rayAngle > 1.5 * Math.PI;
+        this.isRayFacingLeft    = !this.isRayFacingRight;
+    }
+
+    cast(columnId)
+    {
+        var xIntercept, yIntercept;
+        var xStep, yStep;
+        ////////////////////////////////////////////
+        /// HORIZONTAL RAY-GRID INTERSECTION CODE
+        ////////////////////////////////////////////
+        
+        // Find the y-coordinate of the closest horizontal grid intersection
+        yIntercept = Math.floor(player.y / TILE_SIZE) * TILE_SIZE;
+        yIntercept += this.isRayFacingDown ? TILE_SIZE : 0;
+
+        console.log("isRayFacingRight?", this.isRayFacingRight);
+        ///////////////////////////////////
+        // Alternative Approach- SAFAK ONOL
+
+        // if (this.isRayFacingUp)
+        // {
+        //     yIntercept = Math.floor(player.y / TILE_SIZE) * TILE_SIZE;
+        // }
+        // else if(this.isRayFacingDown)
+        // {
+        //     yIntercept = Math.ceil(player.y / TILE_SIZE) * TILE_SIZE;
+        // }
+        ///////////////////////////
+
+        // Find the x-coordinate of the closest horizontal grid intersection
+        xIntercept = player.x + ((yIntercept - player.y) / Math.tan(this.rayAngle));
+
+        yStep = TILE_SIZE;
+        yStep *= this.isRayFacingUp ? -1 : 1;
+
+        xStep = TILE_SIZE / Math.tan(this.rayAngle);
+        xStep *= this.isRayFacingLeft && xStep > 0 ? -1 : 1;
+        xStep *= this.isRayFacingRight && xStep < 0 ? -1 : 1;
+    }
+
+    render()
+    {
+        stroke("rgba(0, 255, 0, 0.5)");
+        line
+        (
+            player.x,
+            player.y,
+            player.x + Math.cos(this.rayAngle) * 30,
+            player.y + Math.sin(this.rayAngle) * 30
+        );
+    }
+
+}
 
 
 // ----- GLOBAL VARIABLES ----- //
 var grid = new MyMap();
 var player = new Player();
+var rays = [];
 
 // ----- MAIN FUNCTIONS ----- //
 function keyPressed()
@@ -164,6 +236,39 @@ function keyReleased()
     }
 }
 
+function castAllRays()
+{
+    var columnId = 0;
+
+    // start first ray by subtracting half of the FOV from rotationAngle
+    var rayAngle = player.rotationAngle - (FOV / 2);
+
+    rays = [];
+
+    // loop all columns casting the rays
+    //for (var i = 0; i < NUM_RAYS; i++)
+    for (var i = 0; i < 1; i++)
+    {
+        var ray = new Ray(rayAngle);
+        ray.cast(columnId);
+
+        rays.push(ray); // add the ray to the list of rays
+        rayAngle += FOV / NUM_RAYS;
+        columnId++;
+    }
+
+}
+
+function normalizeAngle(angle)
+{
+    angle = angle % (2 * Math.PI);
+    if (angle < 0)
+    {
+        angle += (2 * Math.PI);
+    }
+    return angle;
+}
+
 // ----- GAME LOOP FUNCTIONS ----- // 
 function setup()
 {
@@ -176,6 +281,7 @@ function update()
 {
     // TODO: update all game objects here
     player.update();  
+    castAllRays();
 }
 
 function draw()
@@ -184,5 +290,12 @@ function draw()
 
     // TODO: render all objects here
     grid.render();
+    
+    
+    for (ray of rays)
+    {
+        ray.render();
+    }
+
     player.render();
 }
